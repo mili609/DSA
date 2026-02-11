474e234b6f22999aa5dbd9309278df3b0b5c4f60
Monali Bundela <monalibundela@gmail.com>
2026-02-11 22:22:47 +0530

    STACK

---
+++ stack_basic.py
@@ -0,0 +1,17 @@
+# Stack using list
+
+stack = []
+
+# push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after push:", stack)
+
+# pop
+stack.pop()
+print("Stack after pop:", stack)
+
+# top element
+print("Top element:", stack[-1])
